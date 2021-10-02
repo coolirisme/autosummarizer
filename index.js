22ed1729a818444129eb3c1c78386f3f2c104d54
Ratul Saha <ratul.s@listertechnologies.com>
2021-10-02 21:32:47 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import { initTables, getPosts, filterPosts, savePosts, getSummaries, postSummaries } from './helpers';
 
-await initTables();
-const res = await getPosts('anime_titties', 'hot');
-const posts = await filterPosts(res);
-const summaries = await getSummaries(posts);
-const repliedPosts = await postSummaries(summaries);
-//await savePosts(repliedPosts);
+let runCount = 0;
+
+const run = async () => {
+  console.log(`Ran ${++runCount} time(s)`);
+
+  await initTables();
+  const res = await getPosts('FreeKarma4U', 'new');
+  const posts = await filterPosts(res);
+  const summaries = await getSummaries(posts);
+  const repliedPosts = await postSummaries(summaries);
+  await savePosts(repliedPosts);
+
+  setTimeout(async () => {
+    await run();
+  }, 300000);
+}
+
+run().then(() => {
+}).catch(async (error) => {
+  console.error(`Error encountered, continuing....`);
+  setTimeout(async () => {
+    await run();
+  }, 300000);
+});
